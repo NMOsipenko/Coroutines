d5cff8df2faae8a1c9850e03506c2e06f7a29ef2
Николай Осипенко <Osipenkonm@intech.rshb.ru>
2025-06-03 18:01:40 +0300

    Test

---
+++ src/main/kotlin/callback/Main.kt
@@ -0,0 +1,16 @@
+package callback
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println
+
+/**
+ * @author Osipenko-NM
+ * mail OsipenkoNM@intech.rshb.ru
+ * ЦК Интеграции (CKI)
+ * @link https://gitlab.rshbdev.ru/rshbintech/integrations/cki/
+ */
+
+class Main {
+    fun main() {
+        println("Hello")
+    }
+}
